ee09b2ec3ce69f27355615ed88ab5e2812e2edfe
Nicholas DiPiazza <nicholas.dipiazza@lucidworks.com>
2018-05-09 12:28:35 -0500

    try setting log4net.config manually

--- SpCrawler/Util.cs
+++ SpCrawler/Util.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+
 namespace SpPrefetchIndexBuilder {
   public class Util {
     public static string addSlashToUrlIfNeeded(string siteUrl) {
@@ -6,6 +9,14 @@ namespace SpPrefetchIndexBuilder {
         siteUrl = siteUrl.Substring(0, siteUrl.Length - 1);
       }
       return siteUrl;
+    }
+    public static string AssemblyDirectory {
+      get {
+        string codeBase = Assembly.GetExecutingAssembly().CodeBase;
+        UriBuilder uri = new UriBuilder(codeBase);
+        string path = Uri.UnescapeDataString(uri.Path);
+        return Path.GetDirectoryName(path);
+      }
     }
   }
 }
